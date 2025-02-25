33d901b60f2c600ea8c735d70a9d435cfde1a6c4
Safi105 <florian.schafitel@me.com>
2025-02-25 20:48:41 +0100

    Update vaillantx6.h

--- esphome/components/vaillantx6/vaillantx6.h
+++ esphome/components/vaillantx6/vaillantx6.h
@@ -1,54 +1,19 @@
-#ifndef VAILLANTX6_H
-#define VAILLANTX6_H
+#pragma once
 
-#include "esphome.h"
-#include <string>
+#include "esphome/core/component.h"
+#include "esphome/components/sensor/sensor.h"
+#include "esphome/components/uart/vaillantx6.h"
 
-#define CMD_LENGTH 7
-#define ANSWER_LENGTH 8
-#define RETURN_TYPE_COUNT 3
+namespace esphome {
+namespace empty_uart_sensor {
 
-typedef unsigned char byte;
-
-void logCmd(const char *tag, byte *cmd);
-
-enum VaillantReturnTypes {
-  None = 0,
-  Temperature,
-  SensorState,
-  Bool,
-  Minutes
-};
-
-uint8_t VaillantReturnTypeLength(VaillantReturnTypes t);
-
-float VaillantParseTemperature(byte *answerBuff, uint8_t offset);
-
-int VaillantParseBool(byte *answerBuff, uint8_t offset);
-
-struct VaillantCommand {
-  std::string Name;
-  byte Address;
-  VaillantReturnTypes ReturnType;
-};
-
-extern const VaillantCommand vaillantCommands[];
-extern const byte vaillantCommandsSize;
-
-class Vaillantx6Sensor : public PollingComponent, public UARTDevice {
+class EmptyUARTSensor : public sensor::Sensor, public PollingComponent, public uart::UARTDevice {
  public:
-  Sensor *sensor_{nullptr};
-  BinarySensor *binary_sensor_{nullptr};
-  const VaillantCommand *command_;
-
-  Vaillantx6Sensor(UARTComponent *parent, const VaillantCommand *command, uint32_t update_interval);
-  void set_sensor(Sensor *sensor);
-  void set_binary_sensor(BinarySensor *binary_sensor);
-  byte checksum(byte *data, byte len);
-  bool checksumOk(byte *answerBuff, byte len);
-  int buildPacket(byte *packet, byte address);
-  int sendPacket(byte *answerBuff, byte *packet);
+  void setup() override;
   void update() override;
+  void loop() override;
+  void dump_config() override;
 };
 
-#endif  // VAILLANTX6_H
+}  // namespace empty_uart_sensor
+}  // namespace esphome
